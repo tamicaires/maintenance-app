ca25f99fe1f9d1b6e07f6a9a06bfb418520061a5
tamicaires <tamiresdec@gmail.com>
2024-09-11 19:05:53 -0300

    chore(create-fleet): remove empty line between trailer plate validations
    - Removed an unnecessary empty line between the firstTrailerPlate, secondTrailerPlate, and thirdTrailerPlate validations for better code readability.

--- src/validations/create-fleet.ts
+++ src/validations/create-fleet.ts
@@ -6,17 +6,14 @@ export const createFleetSchema = z.object({
   firstTrailerPlate: z
     .string()
     .regex(/^[A-Z]{3}-\d{4}$/, "Formato de placa inválido")
-
     .or(z.literal("")),
   secondTrailerPlate: z
     .string()
     .regex(/^[A-Z]{3}-\d{4}$/, "Formato de placa inválido")
-
     .or(z.literal("")),
   thirdTrailerPlate: z
     .string()
     .regex(/^[A-Z]{3}-\d{4}$/, "Formato de placa inválido")
-
     .or(z.literal("")),
   km: z.string().refine((val) => val === "" || Number(val) >= 0, {
     message: "KM deve ser maior ou igual a 0",
