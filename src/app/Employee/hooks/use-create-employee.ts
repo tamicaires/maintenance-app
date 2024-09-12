92ad4a2f53e18a82d3f4e4c322fa013da1f00ec1
tamicaires <tamiresdec@gmail.com>
2024-09-12 20:29:16 -0300

    feat: create employee hook

---
+++ src/app/Employee/hooks/use-create-employee.ts
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import { queryClient } from "@/services/query-client";
+import { EmployeeService } from "@/services/employee";
+import { IApiResponse } from "@/services/api";
+import {
+  IEmployee,
+  IEmployeeCreateAndUpdate,
+} from "@/interfaces/employee.interface";
+import {
+  createEmployeeSchema,
+  CreateEmployeeData,
+} from "@/validations/create-employee";
+
+export function useCreateEmployee() {
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const defaultValues: CreateEmployeeData = {
+    name: "",
+    workShift: "",
+    jobTitleId: "",
+    status: "ATIVO",
+  };
+
+  const createEmployeeForm = useForm<CreateEmployeeData>({
+    resolver: zodResolver(createEmployeeSchema),
+    defaultValues,
+  });
+
+  const {
+    handleSubmit,
+    reset,
+    formState: { isSubmitting },
+  } = createEmployeeForm;
+
+  const mutation = useMutation<
+    IApiResponse<IEmployee>,
+    Error,
+    IEmployeeCreateAndUpdate
+  >({
+    mutationFn: EmployeeService.create,
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({ queryKey: ["employees"] });
+      if (response.success && response.data) {
+        console.log("Employee created:", response.data);
+        toast.success("Funcionário criado com sucesso!");
+        setIsSuccess(true);
+      }
+    },
+    onError: (error) => {
+      console.error("Error in mutation:", error);
+      toast.error(error.message || "Ocorreu um erro ao criar o funcionário.");
+    },
+  });
+
+  const submitEmployeeData = (data: CreateEmployeeData) => {
+    mutation.mutate(data);
+  };
+
+  const resetForm = () => {
+    reset();
+    setIsSuccess(false);
+  };
+
+  return {
+    createEmployeeForm,
+    handleSubmit: handleSubmit(submitEmployeeData),
+    isSubmitting,
+    isSuccess,
+    resetForm,
+  };
+}
+
+export type FormFields = keyof CreateEmployeeData;
