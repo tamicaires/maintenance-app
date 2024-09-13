aabafc9188d62ebb88f6d26d7893600710f7e3de
tamicaires <tamiresdec@gmail.com>
2024-09-13 10:46:17 -0300

    fix: fixing services types interface

--- src/app/Order/hooks/use-service-by-order.ts
+++ src/app/Order/hooks/use-service-by-order.ts
@@ -1,10 +1,10 @@
-import { IService } from "@/interfaces/service.interface";
+import { IServiceWithEmployee } from "@/interfaces/service.interface";
 import { IApiResponse } from "@/services/api";
 import { ServicesService } from "@/services/service";
 import { useQuery } from "@tanstack/react-query";
 
 export function useWorkOrderServices(workOrderId: string) {
-  const data = useQuery<IApiResponse<IService[]>>({
+  const data = useQuery<IApiResponse<IServiceWithEmployee[]>>({
     queryKey: ["work-order-services"],
     queryFn: () => ServicesService.getByWorkOrder(workOrderId),
     staleTime: 60 * 5 * 1000,
