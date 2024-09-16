ce063902926fade3c2d1557f3126ba523ab2fcf1
tamicaires <tamiresdec@gmail.com>
2024-09-16 19:44:55 -0300

    style: change button ui component text color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-card hover:bg-primary/90",
+        default: "bg-primary text-zinc-100 hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input text-zinc-100 bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-primary border border-primary hover:bg-secondary",
         ghost: "hover:bg-accent hover:text-accent-foreground",
