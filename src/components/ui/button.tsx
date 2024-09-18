5e56cef953d6d810f5d53e71d3e50b674460e0e6
tamicaires <tamiresdec@gmail.com>
2024-09-18 17:41:50 -0300

    feat: add and update some styles on ui components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input text-zinc-100 bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-primary border border-primary hover:bg-secondary",
         ghost: "hover:bg-accent hover:text-accent-foreground",
