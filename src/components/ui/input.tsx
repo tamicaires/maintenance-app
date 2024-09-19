79ebd260da62989878ee8aaecebedcc6dc2d60ac
tamicaires <tamiresdec@gmail.com>
2024-09-19 18:07:16 -0300

    fix: wrong time zone on date inputs

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
 export interface InputProps
@@ -7,14 +6,34 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const handleDateTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (type === "datetime-local" && props.onChange) {
+        const date = new Date(e.target.value);
+        const isoString = new Date(
+          date.getTime() - date.getTimezoneOffset() * 60000
+        ).toISOString();
+        const event = {
+          ...e,
+          target: {
+            ...e.target,
+            value: isoString,
+          },
+        };
+        props.onChange(event as React.ChangeEvent<HTMLInputElement>);
+      } else if (props.onChange) {
+        props.onChange(e);
+      }
+    };
+
     return (
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2  focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
+        onChange={handleDateTimeChange}
         {...props}
       />
     );
