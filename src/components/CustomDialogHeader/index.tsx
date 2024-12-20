e783c532c6867ad8734f71cf228dbbbd1f419b6d
tamicaires <tamiresdec@gmail.com>
2024-12-20 10:27:08 -0300

    style: increase text size

--- src/components/CustomDialogHeader/index.tsx
+++ src/components/CustomDialogHeader/index.tsx
@@ -9,7 +9,7 @@ export function CustomDialogHeader({title, subtitle}: CustomDialogHeaderProps) {
       <div className="text-2xl font-semibold leading-none tracking-tight">
         {title}
       </div>
-      <div className="text-sm text-muted-foreground">
+      <div className="text-[0.920rem] text-muted-foreground">
         {subtitle}
       </div>
     </div>
