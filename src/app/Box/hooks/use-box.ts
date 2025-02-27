59455b22388e932fe989af2d8a75c71a636a65da
tamicaires <tamiresdec@gmail.com>
2025-02-27 10:07:15 -0300

    feat: update queryKeys on boxes hook

--- src/app/Box/hooks/use-box.ts
+++ src/app/Box/hooks/use-box.ts
@@ -2,10 +2,11 @@ import { IBox } from "@/shared/types/box";
 import { IApiResponse } from "@/services/api";
 import { BoxService } from "@/services/box";
 import { useQuery } from "@tanstack/react-query";
+import { QueryKeysEnum } from "@/shared/enums/query-keys";
 
 export function useBoxes() {
   return useQuery<IApiResponse<IBox[]>>({
-    queryKey: ["boxes"],
+    queryKey: [QueryKeysEnum.Box],
     queryFn: () => BoxService.getAll(),
     staleTime: 60 * 5 * 1000,
   });
