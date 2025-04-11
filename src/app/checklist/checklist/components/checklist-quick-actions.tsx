42723edc6611ccbd5e458da8f5df0cca919d9402
tamicaires <tamiresdec@gmail.com>
2025-04-11 15:04:43 -0300

    refactor: Improve ChecklistQuickActions component and add logic for returning to work order

--- src/app/checklist/checklist/components/checklist-quick-actions.tsx
+++ src/app/checklist/checklist/components/checklist-quick-actions.tsx
@@ -1,33 +1,35 @@
 import { format } from "date-fns";
 import { Button } from "@/components/ui/button";
+import { useDialog } from "@/context/dialog";
 
 type QuickActionsProps = {
   lastUpdate?: string;
+  isFromWorkOrder?: boolean;
 };
 
-export function ChecklistQuickActions({ lastUpdate }: QuickActionsProps) {
+export function ChecklistQuickActions({
+  lastUpdate,
+  isFromWorkOrder,
+}: QuickActionsProps) {
+  const { closeDialog,  } = useDialog();
+  if (isFromWorkOrder) {
+    // TODO: Implementar lógica para voltar para a ordem de serviço ao finalizar checklist
+  }
   return (
-    <div className="fixed bottom-0 right-[0.115rem] w-full border-t bg-background/80 backdrop-blur-sm p-4 rounded-b-lg">
+    <div className="fixed bottom-0 right-[0.112rem] w-full border-t bg-background/80 backdrop-blur-sm p-4 rounded-b-lg">
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-4">
           <div className="flex items-center gap-2">
-            {/* <Avatar className="h-8 w-8">
-              <AvatarImage
-                src={`https://api.dicebear.com/7.x/initials/svg?seed=${1245}`}
-              />
-              <AvatarFallback>LU</AvatarFallback>
-            </Avatar> */}
             <div className="text-sm">
               <p className="font-medium">Última atualização</p>
               <p className="text-muted-foreground">
                 {lastUpdate && format(new Date(lastUpdate), "dd/MM/yyyy HH:mm")}
-                {/* {format(new Date(), "dd/MM/yyyy HH:mm")} */}
               </p>
             </div>
           </div>
         </div>
         <div className="flex items-center gap-2">
-          <Button variant="outline" onClick={() => {}}>
+          <Button variant="outline" onClick={() => closeDialog()}>
             Fechar
           </Button>
 
