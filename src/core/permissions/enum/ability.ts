c324f838379d74578b4173902a148aad64b3bbfd
tamicaires <tamiresdec@gmail.com>
2025-04-17 16:00:40 -0300

    refactor: Add AbilityEnum for managing user permissions

---
+++ src/core/permissions/enum/ability.ts
@@ -0,0 +1,17 @@
+export enum ActionEnum {
+  Manage = "manage",
+  Create = "create",
+  Read = "read",
+  Update = "update",
+  Delete = "delete",
+  ViewReport = "view report",
+}
+
+export enum SubjectEnum {
+  Company = "Company",
+  User = "User",
+  Carrier = "Carrier",
+  Fleet = "Fleet",
+  Box = "Box",
+  Service = "Service"
+}
