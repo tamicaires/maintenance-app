9f05d2c092bb7534c4864bbba54212b4c54dfa56
tamicaires <tamiresdec@gmail.com>
2025-04-17 16:16:30 -0300

    refactor: Update main.tsx to include Providers and ErrorBoundary components

--- src/main.tsx
+++ src/main.tsx
@@ -10,19 +10,25 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/shared/services/query-client";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { Providers } from "./core/providers/index.tsx";
+import { ErrorBoundary } from "./components/error-boundary/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <TooltipProvider>
-            <DndProvider backend={HTML5Backend}>
-              <App />
-            </DndProvider>
-          </TooltipProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
+      <Providers>
+        {/* <ErrorBoundary> */}
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>
+            <TooltipProvider>
+              <DndProvider backend={HTML5Backend}>
+                <App />
+              </DndProvider>
+            </TooltipProvider>
+          </ThemeProvider>
+        </QueryClientProvider>
+        {/* </ErrorBoundary> */}
+      </Providers>
     </Provider>
   </StrictMode>
 );
