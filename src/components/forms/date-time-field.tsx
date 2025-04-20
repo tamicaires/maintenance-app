7692ae75f6aed4207356702b3b9aafe92f6e4ff9
tamicaires <tamiresdec@gmail.com>
2025-04-20 16:24:14 -0300

    chore: remove client message

--- src/components/forms/date-time-field.tsx
+++ src/components/forms/date-time-field.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useFormContext } from "react-hook-form"
