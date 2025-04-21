caca3af1a6e7762c23e7b04389731f0c5a9404ad
tamicaires <tamiresdec@gmail.com>
2025-04-21 13:39:35 -0300

    refactor: update use part request bywork order hook to use new part request service

--- src/features/part-request/hooks/use-part-request-by-work-order-id.ts
+++ src/features/part-request/hooks/use-part-request-by-work-order-id.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { IApiResponse } from "@/shared/services/api";
 import { IPartRequest } from "@/shared/types/part-request";
-import { PartRequestService } from "@/shared/services/part-request";
+import { partRequestService } from "@/shared/services/part-request-service/part-request";
+import { QueryKeysEnum } from "@/shared/enums/query-keys";
 
 export function usePartRequestByWorkOrderId(workOrderId: string) {
-  return useQuery<IApiResponse<IPartRequest[]>>({
-    queryKey: ["part-requests", workOrderId],
-    queryFn: () => PartRequestService.getByWorkOrderId(workOrderId),
+  return useQuery<IPartRequest[]>({
+    queryKey: [QueryKeysEnum.Part_Request, workOrderId],
+    queryFn: () => partRequestService.getByWorkOrderId(workOrderId),
     staleTime: 60 * 5 * 1000,
   });
 }
