cf1fc897fa94eabe16abc1e8a7ae0aa11a92c6eb
tamicaires <tamiresdec@gmail.com>
2025-04-21 13:21:30 -0300

    refactor: refactor use create assgiment hook, to get more clear implementation

--- src/features/service-assigment/hooks/use-create-service-assigment.ts
+++ src/features/service-assigment/hooks/use-create-service-assigment.ts
@@ -1,104 +1,58 @@
-import { useMutation } from "@tanstack/react-query"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useEffect } from "react"
-import { z } from "zod"
+import { useMemo, useState } from "react"
 import { queryClient } from "@/shared/services/query-client"
-import { IApiResponse } from "@/shared/services/api"
-import { ServiceAssignmentService } from "@/shared/services/service-assigment"
-import { ICreateServiceAssigment, IServiceAssignment } from "@/shared/types/service-assigment"
-import { ServiceAssigmentStatus } from "@/shared/enums/service-assigment"
 import { QueryKeysEnum } from "@/shared/enums/query-keys"
+import { createServiceAssigmentDefaultValues, CreateServiceAssignmentSchema, createServiceAssignmentSchema } from "../forms/create-service-assigment-form"
+import { useMutation } from "@/core/api/hooks/use-mutation"
+import { serviceAssignmentService } from "@/shared/services/service-assgiment-service/service-assigment"
 
-const createServiceAssignmentSchema = z.object({
-  workOrderId: z.string().min(2, "É obrigatório está vinculado a uma Ordem Serviço"),
-  serviceId: z.string().min(2, "O campo Serviço é obrigatório"),
-  trailerId: z.string().min(2, "O campo Reboque é obrigatório."),
-  axleId: z.string().nullable(),
-  status: z.nativeEnum(ServiceAssigmentStatus, { errorMap: () => ({ message: "Status inválido" }) }),
-  startAt: z.date().nullable(),
-  endAt: z.date().nullable(),
-})
+export function useCreateServiceAssignment(workOrderId: string) {
+  const [isCreateDialogOpen, setIsCreateDialogOpen] = useState<boolean>(false);
 
-type CreateServiceAssignmentData = z.infer<typeof createServiceAssignmentSchema>
-
-export function useCreateServiceAssignment(
-  setIsDialogOpen: (open: boolean) => void,
-  workOrderId: string,
-  addToast: (toast: any) => void
-) {
-  const defaultValues: CreateServiceAssignmentData = {
-    workOrderId: workOrderId,
-    serviceId: "",
-    trailerId: "",
-    axleId: null,
-    status: ServiceAssigmentStatus.PENDING,
-    startAt: null,
-    endAt: null,
-  }
-
-  const createServiceAssignmentForm = useForm<CreateServiceAssignmentData>({
+  const form = useForm({
     resolver: zodResolver(createServiceAssignmentSchema),
-    defaultValues,
+    defaultValues: {
+      ...createServiceAssigmentDefaultValues,
+      workOrderId: workOrderId,
+    }
   })
+  const values = form.watch();
+  const statusWatcher = form.watch("status");
+  const trailerIdWatcher = form.watch("trailerId");
 
-  const {
-    handleSubmit,
-    reset,
-    formState: { isSubmitting, isLoading },
-  } = createServiceAssignmentForm
-
-  const {
-    mutate: mutateCreate,
-    isSuccess,
-    isError,
-    isPending,
-    data,
-    error,
-  } = useMutation<IApiResponse<IServiceAssignment>, Error, ICreateServiceAssigment>({
-    mutationFn: ServiceAssignmentService.create,
-    onSuccess: (response) => {
+  const { mutate: createServiceAssigment, isPending } = useMutation(
+    (data: CreateServiceAssignmentSchema) => serviceAssignmentService.create(data), {
+    successMessage: "Designação de serviço realizada com sucesso!",
+    onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QueryKeysEnum.Service_Assigment] })
-      if (response.success && response.data) {
-        addToast({
-          type: "success",
-          title: "Serviço adicionado com sucesso!",
-          message: "Serviço já vinculado a ordem de serviço",
-          duration: 4000,
-        })
-      }
-    },
-    onError: (error) => {
-      console.error("Error in mutation:", error)
-      addToast({
-        type: "error",
-        title: "Erro ao adicionar serviço",
-        message: error.message || "Ocorreu um erro ao adicionar serviço.",
-        duration: 5000,
-      })
-    },
-  })
+      handleClose()
+    }
+  });
 
-  const submitServiceAssignmentData = (data: CreateServiceAssignmentData) => {
-    mutateCreate(data)
-    reset()
+  const handleSubmit = form.handleSubmit(
+    (data) => {
+      createServiceAssigment(data)
+    }
+  )
+
+  const handleClose = () => {
+    setIsCreateDialogOpen(false)
+    form.reset()
   }
 
-  useEffect(() => {
-    if (isSuccess) {
-      setIsDialogOpen(false)
-    }
-  }, [isSuccess, setIsDialogOpen])
+  const canSubmit = useMemo(() => {
+    return createServiceAssignmentSchema.safeParse(values).success;
+  }, [values])
 
   return {
-    createServiceAssignmentForm,
-    handleSubmit: handleSubmit(submitServiceAssignmentData),
-    isSubmitting,
-    isLoading,
-    isError,
+    form,
+    handleSubmit,
     isPending,
-    error,
-    data,
-    reset,
-  }
+    canSubmit,
+    statusWatcher,
+    trailerIdWatcher,
+    isCreateDialogOpen,
+    setIsCreateDialogOpen
+  };
 }
