c610ee1e1228bddd0ad46b7a5e9419acf6dcab3f
tamicaires <tamiresdec@gmail.com>
2025-04-25 19:06:05 -0300

    refactor: Update useChecklistItems to use checklistItemService for fetching checklist items

--- src/features/checklist/checklist-item/hooks/use-checklist-items.ts
+++ src/features/checklist/checklist-item/hooks/use-checklist-items.ts
@@ -1,13 +1,12 @@
-import type { IApiResponse } from "@/shared/services/api"
 import { useQuery } from "@tanstack/react-query"
 import type { IChecklistItem } from "@/shared/types/checklist"
-import { ChecklistItemService } from "@/shared/services/checklist/checklist-item"
 import { QueryKeysEnum } from "@/shared/enums/query-keys"
+import { checklistItemService } from "@/shared/services/checklist/checklist-item-service"
 
 export function useChecklistItems(checklistId: string) {
-  const { data, isLoading, error } = useQuery<IApiResponse<IChecklistItem[]>>({
+  const { data, isLoading, error } = useQuery<IChecklistItem[]>({
     queryKey: [QueryKeysEnum.ChecklistItem, checklistId],
-    queryFn: () => ChecklistItemService.getByChecklist(checklistId),
+    queryFn: () => checklistItemService.getByChecklist(checklistId),
     staleTime: 60 * 5 * 1000,
   })
 
