fc34a7458945ac69f58fc15a8cd05250e4b62af9
tamicaires <tamiresdec@gmail.com>
2025-04-25 14:34:30 -0300

    refactor: Optimize BoxesView component with lazy loading for images and improve performance

--- src/features/boxes/components/boxes-view/boxes-view.tsx
+++ src/features/boxes/components/boxes-view/boxes-view.tsx
@@ -3,9 +3,10 @@ import { useActiveBoxes } from "../../hooks/use-active-boxes";
 import { IBoxWithRelationalData } from "@/shared/types/box";
 import { MaintenanceCard } from "@/features/workshop-dashboard/components/maintenance-card";
 import { getDataOrDefault } from "@/utils/data";
-import { Spinner } from "@/components/Spinner";
 import { ReportTable } from "@/components/data-table/data-table";
 import { boxesColumns } from "../../data/boxes-collums";
+import { LoadingOverlay } from "@/components/loading-overlay";
+import EmptyState from "@/components/states/empty-state";
 
 type BoxesSectionProps = {
   viewMode: "grid" | "list";
@@ -15,37 +16,44 @@ export function BoxesView({ viewMode }: BoxesSectionProps) {
   const { data, isLoading } = useActiveBoxes();
   const boxes = getDataOrDefault<IBoxWithRelationalData[]>(data, []);
 
-  if (isLoading) {
-    return <Spinner />;
-  }
+  const renderListView = () => (
+    <ReportTable
+      columns={boxesColumns}
+      data={boxes}
+      isloadingData={isLoading}
+      totalItems={boxes.length}
+      onPaginationChange={() => {}}
+    />
+  );
 
-  if (viewMode === "list") {
-    return (
-      <ReportTable
-        columns={boxesColumns}
-        data={boxes}
-        isloadingData={isLoading}
-        totalItems={boxes.length}
-        onPaginationChange={() => {}}
-      />
-    );
-  }
+  const renderGridView = () => (
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+      {boxes.map((box) => (
+        <div key={box.id} className="h-full">
+          {box.workOrder ? (
+            <MaintenanceCard box={box} />
+          ) : (
+            <EmptyCard boxNumber={box.name} />
+          )}
+        </div>
+      ))}
+    </div>
+  );
 
-  if (viewMode === "grid") {
-    return (
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-        {boxes.map((box) => (
-          <div key={box.id} className="h-full">
-            {box.workOrder ? (
-              <MaintenanceCard box={box} />
-            ) : (
-              <EmptyCard boxNumber={box.name} />
-            )}
-          </div>
-        ))}
-      </div>
-    );
-  }
+  const renderEmptyState = () => (
+    <EmptyState
+      message="Nenhum box encontrado"
+      description="Verifique se possui boxes cadastrados"
+    />
+  );
 
-  return null;
+  return (
+    <LoadingOverlay isLoading={isLoading}>
+      {boxes.length === 0
+        ? renderEmptyState()
+        : viewMode === "list"
+        ? renderListView()
+        : renderGridView()}
+    </LoadingOverlay>
+  );
 }
